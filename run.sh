5916030847bb631fb3ddc6e139c56e23fe6fef12
runtask <runtask@fastmail.com>
2019-03-07 12:27:22 +0800

    Force sync master branch to other branches in master branch CI

--- run.sh
+++ run.sh
@@ -6,15 +6,32 @@ set -e
 
 echo "TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE"
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
+echo "TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR"
+
+
+if [ ! -d "$TRAVIS_BUILD_DIR" ]; then
+    echo "ERROR: TRAVIS_BUILD_DIR not exists or not a directory: $TRAVIS_BUILD_DIR"
+    exit 1
+fi
+
 
 case "$TRAVIS_BRANCH" in
 master):
     # Currently we are on master branch
     # Force sync master branch to other branches: daily, weekly, monthly
+    TMPREPO="/tmp/$(cat /dev/random | tr -dc 'a-zA-Z0-9' | head -c 16)"
+    git clone "https://runtask:$GITHUB_PAT@github.com/runtask/runtask" "$TMPREPO"
+    pushd "$TMPREPO"
     for branch in daily weekly monthly; do
         echo "INFO: Force sync master branch to $branch"
+        git checkout "$branch"
+        git reset --hard origin/master
+        git push -f origin "$branch"
     done
+    popd
     ;;
+
 daily|weekly|monthly):
     # Only do "api" build and "cron" build
     if [ "$TRAVIS_EVENT_TYPE" = "api" ] || [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
@@ -24,6 +41,7 @@ daily|weekly|monthly):
         echo "INFO: Ignore build for $TRAVIS_BRANCH (reason: $TRAVIS_EVENT_TYPE)"
     fi
     ;;
+
 *):
     echo "ERROR: Unknown branch: $TRAVIS_BRANCH"
     exit 1
