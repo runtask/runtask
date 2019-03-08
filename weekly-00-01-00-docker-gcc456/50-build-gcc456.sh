ee03807adde06c67be7265306a842a3b318fcadf
runtask <runtask@fastmail.com>
2019-03-08 14:30:06 +0800

    Add weekly task: docker-gcc from 4 to 8

---
+++ weekly-00-01-00-docker-gcc456/50-build-gcc456.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+source ../common/common.bashrc
+
+export DOCKER_PUSH=true
+
+GCC_VERSION=4.8 run ../common/docker-build-gcc/build.sh
+GCC_VERSION=4.9 run ../common/docker-build-gcc/build.sh
+
+GCC_VERSION=5.3 run ../common/docker-build-gcc/build.sh
+GCC_VERSION=5.4 run ../common/docker-build-gcc/build.sh
+GCC_VERSION=5.5 run ../common/docker-build-gcc/build.sh
+
+GCC_VERSION=6.3 run ../common/docker-build-gcc/build.sh
+GCC_VERSION=6.4 run ../common/docker-build-gcc/build.sh
+GCC_VERSION=6.5 run ../common/docker-build-gcc/build.sh
